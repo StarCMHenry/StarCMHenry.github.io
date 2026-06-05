61d046443cfcf14f921da37b4b86d614a0398273
StarCMHenry <smbphjfv5f@privaterelay.appleid.com>
2026-06-06 00:14:33 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-﻿const CACHE = "sb-v1";
-const FILES = ["salary_calculator.html","manifest.json","index.png","icon-180.png","icon-192.png","icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
-self.addEventListener("activate",e=>e.waitUntil(clients.claim()));
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
